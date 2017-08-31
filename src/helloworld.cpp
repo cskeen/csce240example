a39916b4f7430b1a0243d5daba66dfa034c7c58c
Chris Skeen <skeen.c@gmail.com>
2017-08-31 13:20:53 -0400

    Added some actual code.

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,1 +1,7 @@
-# This is the skeleton of a Hello World Program
+//This is the skeleton of a Hello World Program
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello, World! This is some code synced with Git." << std::endl;
+}
